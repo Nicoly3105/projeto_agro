5b3c5a2d56b11f6c5ba796cb786237a3f6d66767
Nicoly3105 <nicolyestudos3105@gmail.com>
2026-05-17 13:24:43 -0300

    Concluindo merge

--- menudocliente.py
+++ menudocliente.py
@@ -4,26 +4,25 @@ produtos = []
 
 while True:
     print('~' * 100)
-    print('~' * 43, 'MENU CLIENTE', '~' * 43)
+    print('~' *43,'MENU CLIENTE', '~' *43)
     print('~' * 100)
 
-    print('[1] Ver produtos')
-    print('[2] Comprar produtos')
-    print('[3] Encomendar produtos')
-    print('[4] Ver encomendas')
-    print('[5] Voltar')
+    print('[1] VER PRODUTOS')
+    print('[2] COMPRAR PRODUTOS')
+    print('[3] ENCOMENDAR PRODUTOS')
+    print('[4] VER ENCOMENDAS')
+    print('[5] HISTÓRICOS DE COMPRAS')
+    print('[6] BENEFÍCIOS DO CLIENTE')
+    print('[7] VOLTAR')
 
     op_cliente = int(input('Digite a opção desejada: '))
 
-    # VER PRODUTOS
     if op_cliente == 1:
-
         if len(produtos) == 0:
             print('Nenhum produto cadastrado!')
 
         else:
-            print('~' * 35, 'LISTA DE PRODUTOS', '~' * 35)
-
+            print('~' *35,'LISTA DE PRODUTOS', '~' *35)
             for produto, quantidade in produtos:
                 print(f'Produto: {produto}')
                 print(f'Quantidade: {quantidade}')
@@ -31,6 +30,7 @@ while True:
 
     # COMPRAR PRODUTOS
     elif op_cliente == 2:
+        print('~' *35,'COMPRAR PRODUTOS', '~' *35)
         produto = input('Digite o produto que deseja comprar: ')
         quantidade = int(input('Digite a quantidade desejada: '))
 
@@ -39,10 +39,12 @@ while True:
 
         print('Compra realizada com sucesso!')
 
+
     # ENCOMENDAR PRODUTOS
     elif op_cliente == 3:
+        print('~' *35,'ENCOMENDAR PRODUTOS', '~' *35)
         produto = input('Digite o produto que deseja encomendar: ')
-        quantidade = float(input('Digite a quantidade desejada: '))
+        quantidade = int(input('Digite a quantidade desejada: '))
         data = input('Digite a data desejada: ')
         horario = input('Digite o horário desejado: ')
 
@@ -57,7 +59,7 @@ while True:
             print('Nenhuma encomenda realizada!')
 
         else:
-            print('~' * 35, 'LISTA DE ENCOMENDAS', '~' * 35)
+            print('~' *35,'LISTA DE ENCOMENDAS', '~' *35)
 
             for produto, quantidade, data, horario in encomendas:
                 print(f'Produto: {produto}')
@@ -66,10 +68,21 @@ while True:
                 print(f'Horário: {horario}')
                 print('-' * 30)
 
-    # VOLTAR
     elif op_cliente == 5:
+        print('~'*35,'HISTÓRICOS DE COMPRAS','~'*35)
+        if len(compras) == 0:
+            print('Nenhuma encomenda realizada!')
+        else:
+            for i in compras:
+                print(i)
+    elif op_cliente == 6:
+        print('~'*35,'BENEFÍCIOS DO CLIENTE','~'*35)
+        if len(compras) >= 3:
+            print('Parabéns! Você possui 10% de desconto nas compras.')
+        else: 
+            faltam = 3 - len(compras)
+            print(f'Faltam {faltam} compras para liberar os 10% de desconto.')
+    elif op_cliente == 7:
         print('Voltando ao menu inicial...')
         break
-
-    else:
-        print('Opção inválida!')
+                   
