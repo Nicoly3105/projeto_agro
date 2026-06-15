527164f3231dbc8127f7ebe0b7ec019dfa472c66
Nicoly3105 <nicolyestudos3105@gmail.com>
2026-06-15 17:48:27 -0300

    versão final

--- completo2.py
+++ completo2.py
@@ -46,10 +46,7 @@ while True:
 
 #MENU ADM
 import menuadm_funcoes
-animais = [ {'Brinco':'002','Animal': 'Vaca','Peso': 37,'Gênero': 'Fêmea','Status': 'Grávida','Valor': 8}]
-produtos = [{'Produto': 'Coalho','Quantidade': 7,'Unidade': 'kg','Valor': 6.5}]
-leite = {'Produto':'Leite','Volume':1000,'Valor':5.5}
-historico = []
+
 if tipo == 'adm':
     
     init()
